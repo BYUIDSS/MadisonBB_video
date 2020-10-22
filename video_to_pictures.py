635e26d6e594c96d504fec648e76fdd7e65b7b25
Tyler Craig <cra18002@byui.edu>
2020-10-21 19:29:02 -0600

    added hello world test code/tested vsc colab

--- video_to_pictures.py
+++ video_to_pictures.py
@@ -6,12 +6,8 @@ import pandas as pd
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input, VGG16
 import numpy as np
-from keras.utils import np_utils
 from skimage.transform import resize
 from sklearn.model_selection import train_test_split
-from keras.models import Sequential
-from keras.layers import Dense, InputLayer, Dropout
-
 # Saving videos as pictures
 count = 0
 videoFile = 'data/thunderridge_at_madison.mp4'
@@ -35,4 +31,6 @@ print("Done baby!")
 img = cv2.imread('photos/frame290.jpg')
 plt.imshow(img)
 plt.show()
+
+print("Hello World")
 
