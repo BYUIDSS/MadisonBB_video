ebeebd29ca0acd800f5635d7c6123411b0b5d91d
Tyler Craig <cra18002@byui.edu>
2020-11-25 18:54:10 -0700

    finished the data organizer and moving files

--- data_organizer.py
+++ data_organizer.py
@@ -2,8 +2,23 @@ import shutil, os
 
 print("current working directory: ", os.getcwd())
 
-if os.path.exists(str(os.getcwd) + "\\dataset\\examples"):
-    print("the path exists!")
+original = "D:\\projects\\MadisonBB_video\\data\\basketballdataset\\examples"
+mp4_path = "D:\\projects\\MadisonBB_video\\data\\basketballdataset\\mp4_files"
+npy_path = "D:\\projects\\MadisonBB_video\\data\\basketballdataset\\npy_files"
+
+if os.path.exists(original) and os.path.exists(mp4_path) and os.path.exists(npy_path):
+    print("The paths exists!")
+
+    for filename in os.listdir(original):
+
+        if filename.endswith(".mp4"):
+            shutil.move(original+"\\"+filename, mp4_path)
+
+        if filename.endswith(".npy"):
+             shutil.move(original+"\\"+filename, npy_path)
+             
+    print("Done moving files!")
 else:
-    print("This path doesn't exist!")
+    print("Paths don't exist")
+
 
